72b4ab3ab7f4d36432111727477a610c21f25e64
lbesson <lionel.besson@gmail.com>
2012-05-07 16:42:06 +0200

    test canvas resize

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ c2capp
 
 To be completed.
 First test if a few features are available
+
+
+todo
+======
+
+i18n
+
+test canvas
+
+test GPS
+
+splash screen / icons
+
+ldpi / mdpi etc
---
+++ assets/js/app.js
@@ -0,0 +1,109 @@
+(function () {
+
+  window.c2corg = window.c2corg || {};
+
+  // Cordova is ready to be used
+  function onDeviceReady() {
+    $('#cpButton').attr('disabled', false);
+    console.log("device ready");
+  }
+
+  window.c2corg.camera = {
+
+    max_thumb_size: 100,
+
+    createThumbnail: function(file, callback) {
+      var img;
+      
+      // Load image
+      img = new Image();
+
+      img.onerror = img.onabort = function() {
+        // Image failed to load
+        callback({success: false});
+      };
+
+      img.onload = function() {
+        var canvas, context, data, scale, width, height;
+        var mime = 'image/jpeg';
+
+        // Compute thumbnail size
+        scale = c2corg.camera.max_thumb_size / Math.max(img.width, img.height);
+        width = Math.round(img.width * scale);
+        height = Math.round(img.height * scale);
+
+        // Compute thumbnail with canvas
+        canvas = document.createElement('canvas');
+        canvas.style.display = 'none';
+        document.body.appendChild(canvas);
+        context = canvas.getContext('2d');
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(img, 0, 0, width, height);
+        try {
+          data = canvas.toDataURL(mime, .5);
+        } catch(e) {
+          try {
+            data = canvas.toDataURL(mime);
+          } catch(E) {
+            data = 'data:image/gif;base64,R0lGODlhGQALAKEAAAAAAP///wAAAAAAACH5BAEKAAIALAAAAAAZAAsAAAIpFI6pe8YPo2ShnhqQvTtlkH3hBUIiuGFjp5FjWZ4KRsY1O+W65OyLUAAAOw==';
+          }
+        }
+
+        // Remove canvas from DOM to free up memory
+        // and execute callback with thumbnail data uri
+        canvas.parentNode.removeChild(canvas);
+        callback({success: true, datauri: data});
+      };
+
+      img.src = file;
+    },
+
+    onCaptureFail: function(message) {
+      alert('Failed because: ' + message);
+    },
+
+    onCaptureSuccess: function(file) {
+      this.createThumbnail(file, function(res) {
+        if (res.success) {
+          // display thumbnail
+          $('#images').append('<img src="' + res.datauri + '" />');
+        } else {
+          this.onCaptureFail('could not resize image');
+        }
+      });
+    },
+
+    capturePhoto: function() {
+      // for testing without phonegap
+      if (!!navigator.camera) {
+        // Take picture using device camera and retrieve image file location
+        navigator.camera.getPicture(this.onCaptureSuccess, this.onCaptureFail,
+          { quality: 25,
+            sourceType: navigator.camera.destinationType.FILE_URI,
+            destinationType: navigator.camera.pictureSource.CAMERA });
+      } else {
+        this.onCaptureSuccess('assets/img/mouchillon.jpg');
+      }
+    }
+  };
+
+  // Wait for Cordova to connect with the device
+  document.addEventListener("deviceready", c2corg.onDeviceReady, false);
+
+})();
+
+
+
+function onPhotoURISuccess(imageURI) {
+  console.log(imageURI);
+
+  // create the image 'thumbnail'
+
+  // display thumbnail
+  $('#images').append('<img style="width:50px;height:50px" src="' +
+    imageURI + '" />');
+}
+
+
+
