da78a9561db1a8aaaac111f0b2b2ed28c83d5809
lbesson <lionel.besson@gmail.com>
2012-08-28 20:14:12 +0200

    typo + missing dep

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@
 /* Application level module
    which depends on filters, directives and services */
 
-angular.module('c2capp', ['c2capp.filters']).
+angular.module('c2capp', ['c2capp.i18n', 'c2capp.filters']).
   config(['$routeProvider', function($routeProvider) {
-  	$routeProvider.
-  	  when('/home', { templateUrl: 'partials/home.html' }).
-  	  when('/gps', { templateUrl: 'partials/gps.html' }).
-  	  when('/images', { templateUrl: 'partials/images.html' }).
-          when('/sync', { templateUrl: 'partials/sync.html' }).
-  	  otherwise({ redirectTo: '/home' });
+    $routeProvider.
+      when('/home', { templateUrl: 'partials/home.html' }).
+      when('/gps', { templateUrl: 'partials/gps.html' }).
+      when('/images', { templateUrl: 'partials/images.html' }).
+      when('/sync', { templateUrl: 'partials/sync.html' }).
+      otherwise({ redirectTo: '/home' });
   }]);
